f1bca89ccf38961ba7c85f856cebc936b146c03f
gene-999 <eugenebaidoo5@gmail.com>
2024-06-26 23:54:16 +0000

    commit for app.js

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image } from 'react-native';
+import { ThemeProvider, useTheme } from './themeControl';
+import HomeScreen from './home'; 
+import SettingsScreen from './settings';
+import MyCardsScreen from './myCards';
+import StatisticsScreen from './statistics';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function AppNavigator() {
+  const { isDarkTheme } = useTheme();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName =  require('./assets/home.png');
+           }
+          else if (route.name === 'My Cards') {
+             iconName =  require('./assets/myCards.png');
+          }
+           else if (route.name === 'Statistics') {
+            iconName =  require('./assets/statistics.png');
+          } 
+          else if (route.name === 'Settings') {
+            iconName =  require('./assets/settings.png');
+          }
+
+          return <Image source={iconName} style={{ width: 25, height: 25, tintColor: focused ? '#257CFF' : 'gray' }} />;
+        },
+        tabBarShowLabel: true,
+        color:"#257CFF",
+        tabBarStyle: {
+          borderWidth: -5,
+          borderTopWidth: 0,
+          height: 60,
+          color: "#257CFF",
+          backgroundColor: isDarkTheme ? '#292941' : 'white',
+        },
+        tabBarInactiveTintColor: isDarkTheme ? 'gray' : 'black',
+        tabBarActiveTintColor: isDarkTheme ? 'lightblue' : 'blue',
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="My Cards" component={MyCardsScreen} />
+      <Tab.Screen name="Statistics" component={StatisticsScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <ThemeProvider >
+      <NavigationContainer >
+        <AppNavigator />
+      </NavigationContainer>
+    </ThemeProvider>
+  );
+}
