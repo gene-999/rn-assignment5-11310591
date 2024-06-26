b3eb28d4b2f1c512adc8641ded82bb06b68ab76d
gene-999 <eugenebaidoo5@gmail.com>
2024-06-26 23:52:05 +0000

    commit for empty stats page

---
+++ statistics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Statistics(){
+    return (
+        <View>
+            <Text>Statistics</Text>
+            <Text>Statistics</Text>
+        </View>
+    )
+}
