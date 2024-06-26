6c5ffc7806816998b505d94cc8fbd443d61fa521
gene-999 <eugenebaidoo5@gmail.com>
2024-06-26 23:51:38 +0000

    commit for empty my cards page

---
+++ myCards.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function MyCard(){
+    return (
+        <View>
+            <Text>My Card</Text>
+            <Text>My Card</Text>
+        </View>
+    )
+}
